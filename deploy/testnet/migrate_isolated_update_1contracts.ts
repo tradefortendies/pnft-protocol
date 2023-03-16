2ec932a8d1c23e7bf827d9b7b952fab5446ecce4
0xkongamoto <hieu.q@autonomous.nyc>
2023-03-16 09:27:52 +0700

    rf miragte script

---
+++ deploy/testnet/migrate_isolated_update_1contracts.ts
@@ -0,0 +1,134 @@
+import fs from "fs";
+
+import bn from "bignumber.js"
+
+import hre, { ethers } from "hardhat";
+
+import { encodePriceSqrt, formatSqrtPriceX96ToPrice } from "../../test/shared/utilities";
+import { AccountBalance, BaseToken, ClearingHouse, ClearingHouseConfig, VPool, GenericLogic, InsuranceFund, MarketRegistry, MockPNFTToken, NftPriceFeed, QuoteToken, RewardMiner, TestERC20, TestFaucet, UniswapV3Pool, Vault, LimitOrderBook, ReferralPayment, NFTOracle, VirtualToken } from "../../typechain";
+import { getMaxTickRange, priceToTick } from "../../test/helper/number";
+import helpers from "../helpers";
+import { formatEther, parseEther, parseUnits } from "ethers/lib/utils";
+const { waitForTx, tryWaitForTx, loadDB, saveDB } = helpers;
+
+import migrateAdmin from "./1_migrate_Admin";
+import migratePriceFeedAll from "./2_migrate_PriceFeed_All";
+import migrateTokens from "./3_migrate_Tokens";
+import migrateQuoteToken from "./4_migrate_QuoteToken";
+import migrateBaseTokenAll from "./5_migrate_BaseToken_All";
+import migrateLibrary from "./6_migrate_Library";
+import migrateUniswapV3 from "./6_migrate_UniswapV3";
+import migrateClearingHouseConfig from "./7_migrate_ClearingHouseConfig";
+import migrateMarketRegistry from "./8_migrate_MarketRegistry";
+import migrateAccountBalance from "./10_migrate_AccountBalance";
+import migrateVPool from "./11_migrate_VPool";
+import migrateInsuranceFund from "./12_migrate_InsuranceFund";
+import migrateVault from "./13_migrate_Vault";
+import migrateClearingHouse from "./15_migrate_ClearingHouse";
+import migratePNFTToken from "./20_migrate_PNFTToken";
+import migrateRewardMiner from "./21_migrate_RewardMiner";
+import migrate_ReferralPayment from "./22_migrate_ReferralPayment";
+import migrate_LimitOrderBook from "./23_migrate_LimitOrderBook";
+import migrate_NFTOracle from "./24_migrate_NFTOracle";
+import migrate_VBaseToken from "./25_migrate_VBaseToken";
+import { } from "../../test/helper/clearingHouseHelper";
+import { BigNumber, providers } from "ethers";
+import {
+    signTypedData, SignTypedDataVersion, TypedMessage,
+} from "@metamask/eth-sig-util";
+
+
+async function main() {
+    await deploy();
+}
+
+export default deploy;
+
+async function deploy() {
+    console.log('START')
+    // import migrateTokens from "./3_migrate_Tokens";
+    {
+        console.log('migrateTokens -- START --')
+        await migrateTokens();
+        console.log('migrateTokens -- END --')
+    }
+    // import migrateQuoteToken from "./4_migrate_QuoteToken";
+    {
+        console.log('migrateQuoteToken -- START --')
+        await migrateQuoteToken();
+        console.log('migrateQuoteToken -- END --')
+    }
+    // import migrateBaseTokenAll from "./5_migrate_BaseToken_All";
+    {
+        console.log('migrateBaseTokenAll -- START --')
+        await migrateBaseTokenAll();
+        console.log('migrateBaseTokenAll -- END --')
+    }
+    // import migrateLibrary from "./6_migrate_Library";
+    {
+        console.log('migrateLibrary -- START --')
+        await migrateLibrary();
+        console.log('migrateLibrary -- END --')
+    }
+    // import migrateClearingHouseConfig from "./7_migrate_ClearingHouseConfig";
+    {
+        console.log('migrateClearingHouseConfig -- START --')
+        await migrateClearingHouseConfig();
+        console.log('migrateClearingHouseConfig -- END --')
+    }
+    // import migrateMarketRegistry from "./8_migrate_MarketRegistry";
+    {
+        console.log('migrateMarketRegistry -- START --')
+        await migrateMarketRegistry();
+        console.log('migrateMarketRegistry -- END --')
+    }
+
+    // import migrateAccountBalance from "./10_migrate_AccountBalance";
+    {
+        console.log('migrateAccountBalance -- START --')
+        await migrateAccountBalance();
+        console.log('migrateAccountBalance -- END --')
+    }
+    // import migrateVPool from "./11_migrate_VPool";
+    {
+        console.log('migrateVPool -- START --')
+        await migrateVPool();
+        console.log('migrateVPool -- END --')
+    }
+    // import migrateInsuranceFund from "./12_migrate_InsuranceFund";
+    {
+        console.log('migrateInsuranceFund -- START --')
+        await migrateInsuranceFund();
+        console.log('migrateInsuranceFund -- END --')
+    }
+    // import migrateVault from "./13_migrate_Vault";
+    {
+        console.log('migrateVault -- START --')
+        await migrateVault();
+        console.log('migrateVault -- END --')
+    }
+    // import migrateClearingHouse from "./15_migrate_ClearingHouse";
+    {
+        console.log('migrateClearingHouse -- START --')
+        await migrateClearingHouse();
+        console.log('migrateClearingHouse -- END --')
+    }
+    // import migrate_NFTOracle from "./24_migrate_NFTOracle";
+    {
+        console.log('migrate_NFTOracle -- START --')
+        await migrate_NFTOracle();
+        console.log('migrate_NFTOracle -- END --')
+    }
+    {
+        console.log('migrate_VBaseToken -- START --')
+        await migrate_VBaseToken();
+        console.log('migrate_VBaseToken -- END --')
+    }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
